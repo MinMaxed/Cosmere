fd7615008ea78639d86ff1c0725719153a1cd24b
Max <gusstorm132@gmail.com>
2018-08-13 11:23:00 -0700

    commit scaffold

---
+++ Cosmere/Cosmere/Magic/Investiture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmere.Magic
+{
+    class Investiture
+    {
+        public string Material { get; set; }
+        public string Consumption { get; set; }
+    }
+}
---
+++ Cosmere/Cosmere/Magic/Mistborn/Allomancy/Misting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cosmere.Magic.Mistborn
+{
+    class Misting
+    {
+        public string Name { get; set; }
+        public string NickName { get; set; }
+        public string Resource { get; set; }
+    }
+}
